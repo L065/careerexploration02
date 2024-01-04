bf206f30ec2f8a45cfb79c92c9c317eba66e6de9
L065 <luffy121020@gmail.com>
2024-01-04 23:21:23 +0800

    新會員註冊寫入資料庫功能: 網頁填表單寫入資料庫功能:在DAO與Controller調整
    1.尚缺詳細會員註冊判斷式的Service 與 可以回到登入前的Referrer頁面
    2.預計下一個功能為 th:if 判斷會員或非會員身分
    3.預計下下一個功能為 維持登入的Session
    
    by Lulu
    20240104

--- src/main/java/com/fivestar/careerexploration02/controller/DBController/UserRegController.java
+++ src/main/java/com/fivestar/careerexploration02/controller/DBController/UserRegController.java
@@ -1,6 +1,8 @@
 package com.fivestar.careerexploration02.controller.DBController;
 
 import com.fivestar.careerexploration02.model.userModel.UserRegModel;
+import com.fivestar.careerexploration02.service.UserRegService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserRegController
 {
+    @Autowired
+    UserRegService userRegService;
+
     @GetMapping("/user_register")
     public String regpage(Model model)
     {
@@ -26,5 +31,38 @@ public class UserRegController
         model.addAttribute("email", userModel.getEmail());
         return "signin";
     }
+
+    //先使用老師的controller
+    @PostMapping("/user_register02")
+    public String UserRegProcess2(@ModelAttribute UserRegModel usermodel, Model model) {
+        String msg = null;
+        // 使用者送 request 進來後 回傳 註冊表單名稱
+        // 呼叫 service 開始進行新增
+        int result = userRegService.Registration(usermodel);
+        switch (result) {
+            case 0:
+                msg = "新增失敗";
+                break;
+            case 1:
+                msg = "您的帳號已經成功註冊完畢";
+                break;
+            case 2:
+                msg = "<h3>您的帳號已經在本系統註冊過,請使用登入功能</h3>";
+                break;
+            case 3:
+                msg = "帳號不可包含系統禁止關鍵字(select,insert,update,delete等 或是惡意字詞)";
+                break;
+            default:
+                msg = "其他原因 請聯絡本站管理人員";
+                break;
+        }
+        // 結果通知
+
+        model.addAttribute("userName",usermodel.getUsername());
+
+
+        return "signin";
+    }
+
 
 }
--- src/main/java/com/fivestar/careerexploration02/service/UserRegService.java
+++ src/main/java/com/fivestar/careerexploration02/service/UserRegService.java
@@ -1,9 +1,45 @@
 package com.fivestar.careerexploration02.service;
 
+import com.fivestar.careerexploration02.DAO_Repository.UserRegDao;
+import com.fivestar.careerexploration02.model.userModel.UserRegModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//先使用老師的Service
 @Service
-public class UserRegService
-{
+public class UserRegService {
 
+    @Autowired
+    UserRegDao userRegDao;
+
+    public int Registration(UserRegModel userModel) {
+        // 1:成功 , 2: 帳號已經存在 3: 包含惡意或是禁用字串
+        // 收到註冊需求  先檢查帳號是否存在
+        // 存在  退申請
+        if (userModel.getUsername().contains("select") || userModel.getUsername().contains("delete")) {
+            return 3;
+        }
+        if( isUserExists(userModel.getUsername())) {
+            return 2;
+        }
+        // 不存在  開始檢查資料是否合規
+        // 過濾 惡意字詞 可能異常字串(select , inser, update, delete 等)
+        // 開始進行 帳號新增作業
+        int cnt = userRegDao.saveUserForm(userModel);
+        if(cnt > 0 ) {
+            return 1;
+        } else {
+            return cnt;
+        }
+    }
+
+    // 獨立寫成一個公用方法 讓確認帳號是否存在的功能可以分享 , 改密碼也需要檢查是有此帳號
+    public boolean isUserExists(String username) {
+        long count = userRegDao.testUserExist(username);
+        if( count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
